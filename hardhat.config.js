c521a650bad20dd98e4fb9c953a30889a5ab664a
Sergio Sánchez Sánchez <sss4esob@gmail.com>
2022-10-30 14:12:23 +0100

    Configure Polygon Mumbai TestNet

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,16 @@
 require("@nomicfoundation/hardhat-toolbox");
+const secret = require("./.secret.json")
+
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {},
+    mumbai: {
+      url: `https://polygon-mumbai.g.alchemy.com/v2/${secret.projectId}`,
+      accounts: [secret.accountPrivateKey]
+    }
+  }
 };
