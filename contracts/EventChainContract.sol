d27dd6322566e44f5de05b28ac7d294b245d6b8c
Sergio Sánchez Sánchez <sss4esob@gmail.com>
2024-06-10 18:49:16 +0200

    Add documentation

--- contracts/EventChainContract.sol
+++ contracts/EventChainContract.sol
@@ -7,6 +7,12 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "./IEventChainContract.sol";
 
+/**
+ * @title EventChainContract
+ * @dev A contract to manage event tickets as non-fungible tokens (NFTs) using the ERC721 standard.
+ *      This contract allows for minting, transferring, and validating event tickets, and provides
+ *      mechanisms to track ownership history and set maximum resale prices.
+ */
 contract EventChainContract is ERC721, ERC721URIStorage, ERC721Burnable, Ownable, IEventChainContract {
 
     uint256 private _tokenIdCounter;
@@ -19,20 +25,31 @@ contract EventChainContract is ERC721, ERC721URIStorage, ERC721Burnable, Ownable
         bool isUsed;
     }
 
-    mapping(uint256 => Ticket) private tickets;
-    mapping(uint256 => uint256) public maxResalePrice;
+    mapping(uint256 => Ticket) private tickets;  // Mapping from token ID to ticket details
+    mapping(uint256 => uint256) public maxResalePrice;  // Mapping from token ID to maximum resale price
 
+    /**
+     * @dev Initializes the contract by setting a `initialOwner` and token name and symbol.
+     * @param initialOwner The address of the initial owner of the contract.
+     */
     constructor(address initialOwner)
         ERC721("EventChainTickets", "ECT")
         Ownable(initialOwner)
     {}
 
+    /**
+     * @dev Safely mints a new ticket and assigns it to `to`.
+     * @param to The address of the recipient.
+     * @param uri The URI of the ticket metadata.
+     * @param eventDetails The details of the event.
+     * @param originalPrice The original price of the ticket.
+     * @param expirationDate The expiration date of the ticket.
+     */
     function safeMint(address to, string memory uri, string memory eventDetails, uint256 originalPrice, uint256 expirationDate) public override {
         uint256 tokenId = _tokenIdCounter;
+        _tokenIdCounter += 1;
         _safeMint(to, tokenId);
         _setTokenURI(tokenId, uri);
-        _tokenIdCounter += 1;
-
         address[] memory emptyHistory = new address[](0);
         tickets[tokenId] = Ticket({
             eventDetails: eventDetails,
@@ -47,6 +64,10 @@ contract EventChainContract is ERC721, ERC721URIStorage, ERC721Burnable, Ownable
         emit TicketMinted(tokenId, to, eventDetails, originalPrice, expirationDate);
     }
 
+    /**
+     * @dev Validates the ticket by marking it as used.
+     * @param tokenId The ID of the ticket to be validated.
+     */
     function validateTicket(uint256 tokenId) public override {
         _requireOwned(tokenId);
         require(!tickets[tokenId].isUsed, "Ticket already used");
@@ -55,17 +76,34 @@ contract EventChainContract is ERC721, ERC721URIStorage, ERC721Burnable, Ownable
         emit TicketValidated(tokenId, msg.sender);
     }
 
+    /**
+     * @dev Returns the ownership history of a ticket.
+     * @param tokenId The ID of the ticket.
+     * @return An array of addresses representing the ownership history.
+     */
     function getTicketHistory(uint256 tokenId) public view override returns (address[] memory) {
         _requireOwned(tokenId);
         return tickets[tokenId].ownershipHistory;
     }
 
+    /**
+     * @dev Returns the status of a ticket.
+     * @param tokenId The ID of the ticket.
+     * @return isUsed Whether the ticket has been used.
+     * @return isValid Whether the ticket is still valid.
+     */
     function getTicketStatus(uint256 tokenId) public view override returns (bool isUsed, bool isValid) {
         _requireOwned(tokenId);
         isUsed = tickets[tokenId].isUsed;
         isValid = _isTicketValid(tokenId);
     }
 
+    /**
+     * @dev Updates the metadata of a ticket.
+     * @param tokenId The ID of the ticket to be updated.
+     * @param newEventDetails The new event details.
+     * @param newURI The new metadata URI.
+     */
     function updateTicketMetadata(uint256 tokenId, string memory newEventDetails, string memory newURI) public override onlyOwner {
         _requireOwned(tokenId);
         tickets[tokenId].eventDetails = newEventDetails;
@@ -73,12 +111,21 @@ contract EventChainContract is ERC721, ERC721URIStorage, ERC721Burnable, Ownable
         emit TicketMetadataUpdated(tokenId, newEventDetails, newURI);
     }
 
+    /**
+     * @dev Sets the maximum resale price for a ticket.
+     * @param tokenId The ID of the ticket.
+     * @param maxPrice The maximum resale price.
+     */
     function setMaxResalePrice(uint256 tokenId, uint256 maxPrice) public override onlyOwner {
         _requireOwned(tokenId);
         maxResalePrice[tokenId] = maxPrice;
         emit TicketMaxResalePriceSet(tokenId, maxPrice);
     }
 
+    /**
+     * @dev Burns tickets that have expired.
+     * @param tokenId The ID of the ticket to be burned.
+     */
     function burnExpiredTickets(uint256 tokenId) public override onlyOwner {
         _requireOwned(tokenId);
         require(!_isTicketValid(tokenId), "Ticket is still valid");
@@ -86,26 +133,38 @@ contract EventChainContract is ERC721, ERC721URIStorage, ERC721Burnable, Ownable
         emit TicketExpired(tokenId);
     }
 
+    /**
+     * @dev Transfers a ticket and updates its ownership history.
+     * @param from The address transferring the ticket.
+     * @param to The address receiving the ticket.
+     * @param tokenId The ID of the ticket to be transferred.
+     */
     function transferWithHistoryUpdate(address from, address to, uint256 tokenId) public override onlyOwner {
          _requireOwned(tokenId);
         require(from != address(0) && to != address(0), "Invalid address");
         require(from == ownerOf(tokenId), "Not the token owner");
 
-        if (from != address(0) && to != address(0)) {
-            tickets[tokenId].ownershipHistory.push(to);
-        }
-
+        tickets[tokenId].ownershipHistory.push(to);
         _transfer(from, to, tokenId);
-
         emit TicketTransferred(tokenId, from, to);
     }
 
+    /**
+     * @dev Internal function to check if a ticket is still valid.
+     * @param tokenId The ID of the ticket.
+     * @return True if the ticket is valid, false otherwise.
+     */
     function _isTicketValid(uint256 tokenId) internal view returns (bool) {
         return (block.timestamp <= tickets[tokenId].expirationDate && !tickets[tokenId].isUsed);
     }
 
     // The following functions are overrides required by Solidity.
 
+    /**
+     * @dev Returns the token URI of a ticket.
+     * @param tokenId The ID of the ticket.
+     * @return The token URI.
+     */
     function tokenURI(uint256 tokenId)
         public
         view
@@ -115,6 +174,11 @@ contract EventChainContract is ERC721, ERC721URIStorage, ERC721Burnable, Ownable
         return super.tokenURI(tokenId);
     }
 
+    /**
+     * @dev Checks if the contract supports a specific interface.
+     * @param interfaceId The interface ID.
+     * @return True if the interface is supported, false otherwise.
+     */
     function supportsInterface(bytes4 interfaceId)
         public
         view
--- contracts/EventChainEventManagerContract.sol
+++ contracts/EventChainEventManagerContract.sol
@@ -5,23 +5,45 @@ import "./IEventChainContract.sol";
 import "./IEventChainEventManagerContract.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
+/**
+ * @title EventChainEventManagerContract
+ * @dev This contract manages the creation and organization of events. It allows event organizers
+ *      to create events, mint tickets, and transfer event ownership. The contract is owned by an 
+ *      owner who can set the address of the associated EventChainContract.
+ */
 contract EventChainEventManagerContract is Ownable, IEventChainEventManagerContract {
 
-    uint256 private _eventIdCounter;
+    uint256 private _eventIdCounter; // Counter to keep track of event IDs
 
-    mapping(uint256 => Event) private events;
-    address _eventChainContractAddress;
+    mapping(uint256 => Event) private events; // Mapping from event ID to event details
+    address _eventChainContractAddress; // Address of the associated EventChainContract
 
+    /**
+     * @dev Initializes the contract by setting a `initialOwner`.
+     * @param initialOwner The address of the initial owner of the contract.
+     */
     constructor(address initialOwner)
         Ownable(initialOwner)
     {}
 
+    /**
+     * @dev Sets the address of the EventChainContract.
+     * @param eventChainContractAddress The address of the EventChainContract.
+     */
     function setEventChainAddress(address eventChainContractAddress) external onlyOwner() {
         _eventChainContractAddress = eventChainContractAddress;
     }
 
+    /**
+     * @dev Creates a new event.
+     * @param name The name of the event.
+     * @param location The location of the event.
+     * @param date The date of the event.
+     * @param ticketPrice The price of a ticket for the event.
+     */
     function createEvent(string memory name, string memory location, string memory date, uint256 ticketPrice) public override {
         uint256 eventId = _eventIdCounter;
+        _eventIdCounter += 1;
         events[eventId] = Event({
             name: name,
             location: location,
@@ -29,20 +51,29 @@ contract EventChainEventManagerContract is Ownable, IEventChainEventManagerContr
             ticketPrice: ticketPrice,
             organizer: msg.sender
         });
-        _eventIdCounter += 1;
-
+       
         emit EventCreated(eventId, name, location, date, ticketPrice, msg.sender);
     }
 
+    /**
+     * @dev Returns the details of an event.
+     * @param eventId The ID of the event.
+     * @return The event details.
+     */
     function getEventDetails(uint256 eventId) public view override returns (Event memory) {
         require(eventId < _eventIdCounter, "Event does not exist");
         return events[eventId];
     }
 
+    /**
+     * @dev Mints a ticket for an event.
+     * @param eventId The ID of the event.
+     * @param to The address of the recipient.
+     * @param uri The URI of the ticket metadata.
+     */
     function mintTicket(uint256 eventId, address to, string memory uri) public override {
         require(eventId < _eventIdCounter, "Event does not exist");
         require(events[eventId].organizer == msg.sender, "Only the event organizer can mint tickets");
-
         require(_eventChainContractAddress != address(0), "Event chain contract address is not set");
         require(address(this).balance == 0, "Cannot mint ticket with pending transactions");
 
@@ -50,6 +81,11 @@ contract EventChainEventManagerContract is Ownable, IEventChainEventManagerContr
         eventChainContract.safeMint(to, uri, events[eventId].name, events[eventId].ticketPrice, block.timestamp + 1 weeks); // Ticket expires in 1 week
     }
 
+    /**
+     * @dev Transfers the ownership of an event.
+     * @param eventId The ID of the event.
+     * @param to The address of the new owner.
+     */
     function transferEvent(uint256 eventId, address to) external onlyOwner {
         Event storage currentEvent = events[eventId];
         require(currentEvent.organizer == msg.sender, "Only the event organizer can transfer the event");
